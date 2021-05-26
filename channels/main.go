f9b47be76a7675bcffe6fb65dfbd7ff63ffccd60
Benji Vesterby <benji@devnw.com>
2021-05-26 06:57:57 -0400

    Fixed up channel example to have runnable main

--- channels/main.go
+++ channels/main.go
@@ -1,9 +1,24 @@
-package channels
+package main
 
 import (
 	"context"
+	"fmt"
 	"time"
 )
+
+func main() {
+	tchan := NewTime(context.Background())
+
+	for i := 0; i < 10; i++ {
+		t, ok := <-tchan
+		if !ok {
+			return
+		}
+
+		fmt.Printf("It's %s and all is well!\n", t.Format("3:04:05PM"))
+		time.Sleep(time.Second)
+	}
+}
 
 func NewTime(ctx context.Context) <-chan time.Time {
 	tchan := make(chan time.Time)
